8f7f2ccca674dcabec0e577388e6ce290a71767f
Safi60 <safi.hanifa06@gmail.com>
2021-01-12 12:46:16 +0100

    View: name,sprites,types,stats of pokemons

--- templates/Pokemons/view.php
+++ templates/Pokemons/view.php
@@ -2,12 +2,13 @@
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Pokemon $pokemon
+ *
  */
 ?>
 <div class="row">
     <aside class="column">
         <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
+            <p class="heading"><?= __('Actions') ?></p>
             <?= $this->Form->postLink(__('Delete Pokemon'), ['action' => 'delete', $pokemon->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemon->id), 'class' => 'side-nav-item']) ?>
             <?= $this->Html->link(__('List Pokemons'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
         </div>
@@ -15,100 +16,119 @@
     <div class="column-responsive column-80">
         <div class="pokemons view content">
             <h3><?= h($pokemon->name) ?></h3>
+            <?php
+                $url = $pokemon['default_front_sprite_url'];
+                $url_back = $pokemon['default_back_sprite_url'];
+                $url_shiny = $pokemon['front_shiny_url'];
+            ?>
+        <div class="slider">
+        <!-- Fleche droite <i id="arrow_right" class="fas fa-arrow-right"></i> -->
+        <figure>
+            <img class="carousel" id="front" src="<?php echo $url; ?>">
+            <img class="carousel" id="shiny" src="<?php echo $url_shiny; ?>">
+            <img class="carousel" id="back"  src="<?php echo $url_back; ?>">
+            <?php
+                foreach ($pokemon->pokemon_types as $pokemonTypes)
+                {
+            ?>
+              <tr>
+                <td>
+                  <h4 class="type"><?php  echo $pokemonTypes['type']['name']; ?></h4>
+                </td>
+              </tr>
+              <?php
+                }
+            ?>
             <table>
-                <tr>
-                    <th><?= __('Name') ?></th>
-                    <td><?= h($pokemon->name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Default Front Sprite Url') ?></th>
-                    <td><?= h($pokemon->default_front_sprite_url) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($pokemon->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Height') ?></th>
-                    <td><?= $this->Number->format($pokemon->height) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Weight') ?></th>
-                    <td><?= $this->Number->format($pokemon->weight) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Pokedex Number') ?></th>
-                    <td><?= $this->Number->format($pokemon->pokedex_number) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($pokemon->created) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Modified') ?></th>
-                    <td><?= h($pokemon->modified) ?></td>
-                </tr>
-            </table>
-            <div class="related">
-                <h4><?= __('Related Pokemon Stats') ?></h4>
-                <?php if (!empty($pokemon->pokemon_stats)) : ?>
-                <div class="table-responsive">
-                    <table>
+                <?php
+                if (!empty($pokemon->pokemon_stats))
+                 {
+                 ?>
+                 <div class="table-responsive">
+                      <?php foreach ($pokemon->pokemon_stats as $pokemonStats)
+                        {
+                         ?>
                         <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Pokemon Id') ?></th>
-                            <th><?= __('Stat Id') ?></th>
-                            <th><?= __('Value') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
+                          <td>
+                            <?php
+                              echo $pokemonStats['stat']['name'];
+                            ?>
+                          </td>
+                          <td><?= h($pokemonStats->value) ?></td>
                         </tr>
-                        <?php foreach ($pokemon->pokemon_stats as $pokemonStats) : ?>
-                        <tr>
-                            <td><?= h($pokemonStats->id) ?></td>
-                            <td><?= h($pokemonStats->pokemon_id) ?></td>
-                            <td><?= h($pokemonStats->stat_id) ?></td>
-                            <td><?= h($pokemonStats->value) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'PokemonStats', 'action' => 'view', $pokemonStats->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'PokemonStats', 'action' => 'edit', $pokemonStats->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'PokemonStats', 'action' => 'delete', $pokemonStats->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemonStats->id)]) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
-                <?php endif; ?>
-            </div>
-            <div class="related">
-                <h4><?= __('Related Pokemon Types') ?></h4>
-                <?php if (!empty($pokemon->pokemon_types)) : ?>
-                <div class="table-responsive">
-                    <table>
-                        <tr>
-                            <th><?= __('Id') ?></th>
-                            <th><?= __('Pokemon Id') ?></th>
-                            <th><?= __('Type Id') ?></th>
-                            <th><?= __('Created') ?></th>
-                            <th><?= __('Modified') ?></th>
-                            <th class="actions"><?= __('Actions') ?></th>
-                        </tr>
-                        <?php foreach ($pokemon->pokemon_types as $pokemonTypes) : ?>
-                        <tr>
-                            <td><?= h($pokemonTypes->id) ?></td>
-                            <td><?= h($pokemonTypes->pokemon_id) ?></td>
-                            <td><?= h($pokemonTypes->type_id) ?></td>
-                            <td><?= h($pokemonTypes->created) ?></td>
-                            <td><?= h($pokemonTypes->modified) ?></td>
-                            <td class="actions">
-                                <?= $this->Html->link(__('View'), ['controller' => 'PokemonTypes', 'action' => 'view', $pokemonTypes->id]) ?>
-                                <?= $this->Html->link(__('Edit'), ['controller' => 'PokemonTypes', 'action' => 'edit', $pokemonTypes->id]) ?>
-                                <?= $this->Form->postLink(__('Delete'), ['controller' => 'PokemonTypes', 'action' => 'delete', $pokemonTypes->id], ['confirm' => __('Are you sure you want to delete # {0}?', $pokemonTypes->id)]) ?>
-                            </td>
-                        </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
-                <?php endif; ?>
-            </div>
+                      <?php
+                      }
+                    }
+                 ?>
+              </table>
+                </figure>
+              </div>
         </div>
-    </div>
 </div>
+
+
+<style>
+ h3 {
+     text-align: center;
+     text-decoration: underline;
+     letter-spacing: 0.05em;
+     padding: 20px;
+     font-family: Arial, Helvetica, sans-serif;
+     font-size: 40px;
+ }
+
+ table {
+   display: inline-block;
+   width: 250px;
+   margin-left:30%;
+   margin-top: 30px;
+   border: 5px solid;
+   padding:30px;
+   border-radius: 20px;
+ }
+
+.type{
+  font-family: "Open Sans Condensed", "Open Sans", helvetica, sans-serif;
+  font-size: 30px;
+  letter-spacing: 0.1em;
+  padding: 10px;
+  border-radius: 10%;
+  border: 5px solid;
+  display: inline-block;
+  margin-top: 50px;
+  margin: 90px;
+  margin-right: 156px;
+  background: linear-gradient(to bottom right,#BCD6E2, #BCD6E2);
+ }
+
+.carousel {
+  border: 1px solid;
+  border-radius: 50%;
+  width: 150px;
+  margin-top: 50px;
+  margin-left: auto;
+  margin-right: auto
+}
+
+#back {
+  background-color: grey;
+  position: relative;
+  margin-left: -250px;
+}
+
+#front , #shiny {
+  background-color: #B1CEB5;
+}
+
+#front {
+  margin-left: 40%;
+}
+
+#shiny {
+  margin-left: 40px;
+}
+
+
+
+
+</style>
