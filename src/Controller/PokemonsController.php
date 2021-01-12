7396ac84ad721d6b8e104db4ad7d8f17fb6a1d8f
Safi60 <safi.hanifa06@gmail.com>
2021-01-12 12:42:03 +0100

    Add Dashboard data

--- src/Controller/PokemonsController.php
+++ src/Controller/PokemonsController.php
@@ -63,4 +63,71 @@ class PokemonsController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
+/**
+     * Delete method
+     *
+     * @param string|null $id Pokemon id.
+     * @return \Cake\Http\Response|null|void Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+
+    public function dashboard()
+    {
+// Doc: https://book.cakephp.org/3/fr/orm/query-builder.html
+
+//  Poids moyen des pokemons de la 4ième génération:
+
+        $moy_quatrieme_gene = $this->Pokemons->find();
+        $moy_quatrieme_gene  ->select(["avgWeight"=>"AVG(Weight)"])
+                             ->where(function ($q2) {
+                             return $q2->between('pokedex_number', 387, 493); 
+                        });
+
+
+
+
+        $nombre_de_fee = $this->Pokemons->find(); 
+        $nombre_de_fee  ->select(["countFairies"=>"COUNT(Pokemons.id)"])
+                        ->matching('PokemonTypes.Types', function ($q3)
+                         {
+                           return $q3->where(['Types.name' => 'fairy']); 
+                         })
+                                     ->where(function ($q3) {
+
+                                            return $q3
+                                                    ->or(function($or){ 
+                                                        return $or
+                                                            ->between('pokedex_number', 1, 151) 
+                                                            ->between('pokedex_number', 252, 386) 
+                                                            ->between('pokedex_number', 722, 796); 
+                                                    });
+
+                                        });
+
+                                        
+// Les 10 premiers pokemons qui possèdent la plus grande vitesse:
+
+        $les_plus_rapides = $this->Pokemons->find();
+        $les_plus_rapides ->select(['name','PokemonStats.value'])
+                          ->matching('PokemonStats.Stats', function ($q) { 
+                          return $q->where(['Stats.name' => 'speed']);
+                 })
+                 ->order(['PokemonStats.value' => 'DESC'])
+                 ->limit(10);
+
+
+
+
+
+        $query=array(
+            "les_plus_rapides"=>$les_plus_rapides,
+            "moy_quatrieme_gene"=>$moy_quatrieme_gene,
+            "nombre_de_fee"=>$nombre_de_fee
+        );
+
+
+
+        $this->set(compact('query')); 
+    }
+
 }
